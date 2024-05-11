e5f3cdbc5d01445304ff39effe3144c3c11044c3
15312011bjtu <61004272+15312011bjtu@users.noreply.github.com>
2024-05-12 00:03:33 +0800

    upgrade
    修改框架，加了允许其他学院的代码。增加了点曲库。

--- js/script.js
+++ js/script.js
@@ -1,32 +1,55 @@
 // JavaScript source code
 
-
-//手动输入文件地址，不能有中文
-const MusicList = [
-    ['FGO.mp3','GoldenLegend.mp3'],//d0是抽卡音效
-    ['Pokemon.mp3','DigitalMonsters.mp3'],//从这里开始是d1
-    ['Yu-Gi-Oh!.mp3','liangerguang.mp3'],
-    ['NARUTO.mp3','pengyou.mp3'],
-    ['jojo2.mp3','bengchemailiu.mp3'],
-    ['JiuPingZhiMaGuan.mp3','JohnWick.mp3'],
-    ['ZeldaQuickMoving.mp3','BOTW_Secret.wav','litang.mp3'],
-    ['NoBulletInGun.mp3','Hail_Hydra.mp3'],
-    ['COC_Sneak.mp3'],
-    ['ThisisSpata.mp3','Vergil.mp3'],
-    ['DaZaiBian.mp3','Smaug.mp3'],
-    ['HeroisNeverDie.mp3','hualiao.mp3'],
-    ['Tekeli-li.mp3','zhugeliang.mp3']    
-    ]
+    /*区分是什么学院的能力or本职能力or其他
+    Bard-本职
+    Lore-逸闻
+    Valor-勇气
+    Glamour-迷惑
+    Swords-剑舞
+    Whispers-低语
+    Creation-创造
+    Eloquence-雄辩
+    Spirits_精魂
+    Others-其他
+    */
 
 
 
 
-//roll D12，投掷一次精魂故事表
+//核心函数，音乐播放
+function PlayMusic() {    
+    //播放音乐
+    //选择音乐列表
+    switch (this['id'].split('_')[0]) {
+        case 'Bard':
+        MusicList = Bard_Spirits;
+        break;
+        case 'Spirits':
+        MusicList = MusicList_Spirits;
+        break;
+    }
+    
+    //选择列表中的音乐
+    let RandomNumber = Math.floor(Math.random() * MusicList[this['id'].split('_')[1]].length);
+    //console.log(RandomNumber)
+    let audioElement = new Audio('./assets/'+ this['id'].split('_')[0] +'/music'+this['id'].split('_')[1]+'/'+MusicList[this['id'].split('_')[1]][RandomNumber]);    
+	audioElement.play();    
+    //振动效果，0.2秒
+    this.classList.add('shake')
+    setTimeout(() => { this.classList.remove('shake') }, 200)
+}
+
+
+
+
+
+//精魂诗人 College of Spirits
+
+//投掷一次精魂故事表
+const Spirits_0 = document.getElementById('Spirits_0');//按钮——骰子
 const row_roll_SpiritTales = document.querySelector(".roll_SpiritTales")//整行
-const d0 = document.getElementById("d0")//按钮——骰子
-const result_SpiritTales = document.getElementById("result_SpiritTales")//结果
-
-function roll() {
+const result_SpiritTales = document.getElementById("result_SpiritTales")//掷骰结果
+function roll_SpiritTales() {
     //随机并显示
     result_SpiritTales.innerHTML = "";
     result_SpiritTales.innerHTML = 'D12 = '+ Math.floor(Math.random()*12+1);
@@ -35,51 +58,58 @@ function roll() {
     setTimeout(() => { row_roll_SpiritTales.classList.remove('shake') }, 500)
 }
 //可能的解决方案 https://blog.csdn.net/sinat_41747081/article/details/90577971
-d0.addEventListener('click', roll)
-d0.addEventListener('click', PlayMusic)
+Spirits_0.addEventListener('click', roll_SpiritTales)
+Spirits_0.addEventListener('click', PlayMusic)
 
 
 
-//根据骰值，播放其文件夹下随机的BGM
-const d1 = document.getElementById('d1');
-const d2 = document.getElementById('d2');
-const d3 = document.getElementById('d3');
-const d4 = document.getElementById('d4');
-const d5 = document.getElementById('d5');
-const d6 = document.getElementById('d6');
-const d7 = document.getElementById('d7');
-const d8 = document.getElementById('d8');
-const d9 = document.getElementById('d9');
-const d10 = document.getElementById('d10');
-const d11 = document.getElementById('d11');
-const d12 = document.getElementById('d12');
 
+//手动输入歌曲列表。不能有中文
+//似乎可以通过github的api获取repo中子目录的内容列表。https://api.github.com/repos/solomonxie/gists/contents/目录名。但是比较麻烦。
+const MusicList_Spirits = [
+    ['FGO.mp3','GoldenLegend.mp3','MagicConch.mp3','xuebao.mp3'],//0是抽卡音效
+    ['Pokemon.mp3','DigitalMonsters.mp3'],//从这里开始是Tale-1
+    ['Yu-Gi-Oh!.mp3','liangerguang.mp3'],
+    ['NARUTO.mp3','pengyou.mp3'],
+    ['jojo2.mp3','bengchemailiu.mp3'],
+    ['JiuPingZhiMaGuan.mp3','JohnWick.mp3'],
+    ['ZeldaQuickMoving.mp3','BOTW_Secret.wav','litang.mp3'],
+    ['NoBulletInGun.mp3','Hail_Hydra.mp3','NeverGonnaGiveYouUp.mp3','MonkeyonTree.mp3','dontshotme.mp3'],
+    ['COC_Sneak.mp3','AssassinsCreed.mp3'],
+    ['ThisisSpata.mp3','Vergil.mp3','wula.mp3'],
+    ['DaZaiBian.mp3','Smaug.mp3'],
+    ['HeroisNeverDie.mp3','hualiao.mp3'],
+    ['Tekeli-li.mp3','zhugeliang.mp3','TenEvenNineTimes.mp3']    
+    ]
 
+//绑定元素
 
-function PlayMusic() {    
-    //播放音乐
-    //console.log(this['id']);
-    let RandomNumber = Math.floor(Math.random() * MusicList[this['id'].slice(1)].length)//array.slice(start, end)如果没有指定end,那么切分的数组包含从 start 到数组结束的所有元素。
-    let audioElement = new Audio('./assets/music'+this['id'].slice(1)+'/'+MusicList[this['id'].slice(1)][RandomNumber]);    
-	audioElement.play();    
-    //振动效果
-    this.classList.add('shake')
-    setTimeout(() => { this.classList.remove('shake') }, 200)
+const Spirits_1 = document.getElementById('Spirits_1');//按钮——放歌
+const Spirits_2 = document.getElementById('Spirits_2');
+const Spirits_3 = document.getElementById('Spirits_3');
+const Spirits_4 = document.getElementById('Spirits_4');
+const Spirits_5 = document.getElementById('Spirits_5');
+const Spirits_6 = document.getElementById('Spirits_6');
+const Spirits_7 = document.getElementById('Spirits_7');
+const Spirits_8 = document.getElementById('Spirits_8');
+const Spirits_9 = document.getElementById('Spirits_9');
+const Spirits_10 = document.getElementById('Spirits_10');
+const Spirits_11 = document.getElementById('Spirits_11');
+const Spirits_12 = document.getElementById('Spirits_12');
+//添加函数响应
+Spirits_1.addEventListener('click', PlayMusic)
+Spirits_2.addEventListener('click', PlayMusic)
+Spirits_3.addEventListener('click', PlayMusic)
+Spirits_4.addEventListener('click', PlayMusic)
+Spirits_5.addEventListener('click', PlayMusic)
+Spirits_6.addEventListener('click', PlayMusic)
+Spirits_7.addEventListener('click', PlayMusic)
+Spirits_8.addEventListener('click', PlayMusic)
+Spirits_9.addEventListener('click', PlayMusic)
+Spirits_10.addEventListener('click', PlayMusic)
+Spirits_11.addEventListener('click', PlayMusic)
+Spirits_12.addEventListener('click', PlayMusic)
 
-}
-
-d1.addEventListener('click', PlayMusic)
-d2.addEventListener('click', PlayMusic)
-d3.addEventListener('click', PlayMusic)
-d4.addEventListener('click', PlayMusic)
-d5.addEventListener('click', PlayMusic)
-d6.addEventListener('click', PlayMusic)
-d7.addEventListener('click', PlayMusic)
-d8.addEventListener('click', PlayMusic)
-d9.addEventListener('click', PlayMusic)
-d10.addEventListener('click', PlayMusic)
-d11.addEventListener('click', PlayMusic)
-d12.addEventListener('click', PlayMusic)
 
 
 
